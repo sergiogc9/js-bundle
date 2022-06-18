31c5a8be095fdbbcbcb47343c5ef8a75bed342a0
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-06-18 15:18:08 +0200

    fix: Use exports field in package.json

--- build.js
+++ build.js
@@ -5,24 +5,38 @@ const humanizeDuration = require('humanize-duration');
 
 const commonConfig = {
 	bundle: true,
-	entryPoints: ['./src/index.ts', './src/cli.ts'],
 	legalComments: 'none',
-	minify: true
+	minify: true,
+	sourcemap: true
 };
 
 const isWatchMode = process.argv.includes('--watch');
 
 const buildPackage = async () => {
 	const startTime = new Date();
-	await build({
-		...commonConfig,
-		format: 'cjs',
-		logLevel: 'silent',
-		outdir: `dist`,
-		platform: 'node',
-		plugins: [nodeExternalsPlugin()],
-		watch: isWatchMode
-	});
+
+	await Promise.all([
+		build({
+			...commonConfig,
+			entryPoints: ['./src/index.ts', './src/cli.ts'],
+			format: 'cjs',
+			logLevel: 'silent',
+			outdir: `dist/cjs/`,
+			platform: 'node',
+			plugins: [nodeExternalsPlugin()],
+			watch: isWatchMode
+		}),
+		build({
+			...commonConfig,
+			entryPoints: ['./src/index.ts'],
+			format: 'esm',
+			logLevel: 'silent',
+			outdir: `dist/esm`,
+			platform: 'node',
+			plugins: [nodeExternalsPlugin()],
+			watch: isWatchMode
+		})
+	]);
 	// eslint-disable-next-line no-console
 	console.log(
 		chalk.greenBright(`⚡ esbuild build done in ${humanizeDuration(new Date().getTime() - startTime.getTime())}`)
